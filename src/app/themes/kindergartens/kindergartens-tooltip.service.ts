c3e13d0ad57b8fd9f17b22a089dd0fc1017fc900
Andrius Geduska <Andrius.Geduska@vplanas.lt>
2020-01-06 10:58:16 +0200

    fix kindegartens tooltip ui position

--- src/app/themes/kindergartens/kindergartens-tooltip.service.ts
+++ src/app/themes/kindergartens/kindergartens-tooltip.service.ts
@@ -38,12 +38,11 @@ export class KindergartensTooltipService {
             // tslint:disable-next-line: max-line-length
             if ((response.results[0].graphic.layer.id === 'feature-darzeliai') && (response.results[0].graphic.layer.id !== 'feature-area')) {
               const top = (event.y + 100) < window.innerHeight ? event.y + 10 + 'px' : event.y - 30 + 'px';
-              const left = (event.x + 100) < window.innerWidth ? event.x + 20 + 'px' : (event.x - 110) + 'px';
+              const left = (event.x + 100) < window.innerWidth ? event.x + 20 + 'px' : (event.x - 80) + 'px';
               const values = response.results['0'];
               const filter = dataStore.mainInfo.filter(data => data.GARDEN_ID === values.graphic.attributes.Garden_Id);
               const textMsg = filter[0].LABEL;
               const text = rend.createText(textMsg);
-              console.log(filter[0].LABEL)
               tooltip.textContent = textMsg;
               rend.appendChild(element.nativeElement, tooltip);
               rend.addClass(tooltip, 'buldings-tooltip')
